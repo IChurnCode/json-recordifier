1d3b5851aad2804fe791a768a285fd9c25faa54e
IChurnCode <rajagopal.iswarya@gmail.com>
2018-01-23 19:22:40 +0530

    flatJson to nestedJson

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-var uniqueRandomArray = require('unique-random-array');
-var names = require('./names.json');
-module.exports = {
-    all: names,
-    random: uniqueRandomArray(names)
+var aggregateJsonRecord = module.exports.aggregateJsonRecord = function (rawData, metricToVisualize, metricsToAggregate) {
+    var imData = {};
+    rawData.forEach(function(curRecord, index, array) {
+      var curItem = imData;
+      for (var i = 0; i < metricsToAggregate.length; i++) {
+        var attrValue = curRecord[metricsToAggregate[i]];
+        if (!curItem[attrValue]) {
+          if (i == metricsToAggregate.length - 1)
+            curItem[attrValue] = curRecord[metricToVisualize];
+          else curItem[attrValue] = {};
+        } else {
+          if (i == metricsToAggregate.length - 1)
+            curItem[attrValue] += curRecord[metricToVisualize];
+        }
+        curItem = curItem[attrValue];
+      }
+      curItem.value = curRecord[metricToVisualize];
+    });
+    return imData;
 };
